d679dc7e33af8d482ff181ce5b324f78a58c9937
antoineludwig <antoine.ludwig60@gmail.com>
2014-05-28 14:45:00 +0200

    Saisie des specs

---
+++ NURL/NURL/Class1.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Ludwig
+ * Date: 28/05/2014
+ * Time: 14:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace NURL
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	[TestFixture]
+	public class Test
+	{
+
+	}
+}
